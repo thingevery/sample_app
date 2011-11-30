87556b2a716671f390ae4698d4891033e898a364
thingevery <dev@thingevery.com>
2011-11-30 11:20:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd953eedcc8df5f8d39e051b738b2a95e33b6a8849f2496dda5d0a50d8eceab2cf2d9193392e3f941b09dc776852d9ccbb93907cf58185367ae4c010e6084b4ec'
